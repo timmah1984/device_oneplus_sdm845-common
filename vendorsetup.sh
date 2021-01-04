f7e0e710a8fb9f265f29b4627a13b636dd53e155
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-04 22:26:36 +0530

    sdm845-common: vendorsetup: Use Source Sepolicy
    This reverts commit 856dc3248ee89adb100e4a4279fcd640a51575b3.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,6 @@ rm -rf hardware/qcom/sdm845/thermal
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/hal
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipacm/src
 rm -rf hardware/qcom/sdm845/data/ipacfg-mgr/ipanat/src
-rm -rf device/corvus/sepolicy
 git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/display
 git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/media
 git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
-git clone https://github.com/chandu078/android_device_corvus_sepolicy.git -b 11 device/corvus/sepolicy
