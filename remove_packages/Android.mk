2b49a6ba2bccb6f44b36648a6be2c153ca17e938
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-07 10:43:59 +0000

    Revert "sdm845-common: Remove GoogleCamera"
    This reverts commit 2329a864bced2a4332bc75030e87a29c8459db10.

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := FilesPrebuilt CarrierSetup ConnMO DCMO Drive FM2 libqcomfm_jni obdm_stub qcom.fmradio Maps MyVerizonServices NfcNci OBDM_Permissions PrebuiltGmail Showcase SprintDM SprintHM TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt
+LOCAL_OVERRIDES_PACKAGES := FilesPrebuilt CarrierSetup ConnMO DCMO Drive FM2 libqcomfm_jni obdm_stub qcom.fmradio Maps MyVerizonServices NfcNci OBDM_Permissions PrebuiltGmail Showcase SprintDM SprintHM TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt GoogleCamera OnePlusCamera OnePlusCameraService
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
