a3aa2889fe69b0347baf66bd2c2e018d0b0e9816
chandu <chandudyavanapelli03@gmail.com>
2021-02-04 15:08:25 +0530

    sdm845-common: Drop vendorsetup Script
    * No need anymore.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++
@@ -1,6 +0,0 @@
-rm -rf hardware/qcom-caf/sdm845/display
-rm -rf hardware/qcom-caf/sdm845/media
-rm -rf hardware/qcom-caf/sdm845/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/display
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/media
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
